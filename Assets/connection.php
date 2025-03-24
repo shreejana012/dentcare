dc762238ff03456149b9c1d8e680b6f6bd734015
Shrijana Poudel <spoude14@my.centennialcollege.ca>
2025-03-24 11:32:13 -0400

    Add ssl cert

--- Assets/connection.php
+++ Assets/connection.php
@@ -6,13 +6,15 @@ $host = getenv('AZURE_MYSQL_HOST') ?: 'localhost';
 $username = getenv('AZURE_MYSQL_USERNAME') ?: 'root';
 $password = getenv('AZURE_MYSQL_PASSWORD') ?: '';
 $dbname = getenv('AZURE_MYSQL_DBNAME') ?: 'dentcare';
+$ssl_ca = '/home/site/wwwroot/ssl/DigiCertGlobalRootCA.crt.pem';
 
 // Create connection
-$conn = new mysqli($host, $username, $password, $dbname);
+$conn = mysqli_init();
+mysqli_ssl_set($conn, NULL, NULL, $ssl_ca, NULL, NULL);
 
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if (!mysqli_real_connect($conn, $host, $username, $password, $dbname, 3306, NULL, MYSQLI_CLIENT_SSL)) {
+    die("Connection failed: " . mysqli_connect_error());
 }
 
 // Optional: Display a success message (for testing only, remove in production)
